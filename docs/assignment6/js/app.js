6b749dc2c7d12ff3dc5316b80dab0010e725f381
Nathaniel Foote <jnfoote76@gmail.com>
2025-03-04 10:22:25 -0500

    Assignment 6 Using .$inject call to protect against minification

--- docs/assignment6/js/app.js
+++ docs/assignment6/js/app.js
@@ -3,29 +3,32 @@
 
     angular.module('LunchCheck', [])
 
-        .controller('LunchCheckController', function ($scope) {
-            $scope.lunchMenuStr = "";
-            $scope.message = "";
-            $scope.messageBorder = "none";
-            $scope.messageFontColor = "green";
+        .controller('LunchCheckController', LunchCheckController);
 
-            $scope.checkIfTooMuch = function () {
-                if ($scope.lunchMenuStr === "") {
-                    $scope.message = "Please enter data first";
-                    $scope.messageBorder = "2px solid red";
-                    $scope.messageFontColor = "red";
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.lunchMenuStr = "";
+        $scope.message = "";
+        $scope.messageBorder = "none";
+        $scope.messageFontColor = "green";
+
+        $scope.checkIfTooMuch = function () {
+            if ($scope.lunchMenuStr === "") {
+                $scope.message = "Please enter data first";
+                $scope.messageBorder = "2px solid red";
+                $scope.messageFontColor = "red";
+            } else {
+                var lunchMenu = $scope.lunchMenuStr.split(",");
+                if (lunchMenu.length <= 3) {
+                    $scope.message = "Enjoy!";
                 } else {
-                    var lunchMenu = $scope.lunchMenuStr.split(",");
-                    if (lunchMenu.length <= 3) {
-                        $scope.message = "Enjoy!";
-                    } else {
-                        $scope.message = "Too much!";
-                    }
-
-                    $scope.messageBorder = "2px solid green";
-                    $scope.messageFontColor = "green";
+                    $scope.message = "Too much!";
                 }
-            };
-        });
 
+                $scope.messageBorder = "2px solid green";
+                $scope.messageFontColor = "green";
+            }
+        };
+    }
 })();
