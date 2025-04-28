22e9a77f079ed465c4963898bfc21c31058a5feb
Nathaniel Foote <jnfoote76@gmail.com>
2025-04-27 20:05:35 -0400

    Assignment 10 Adding unit tests

---
+++ docs/assignment10/spec/sign-up.controller.spec.js
@@ -0,0 +1,66 @@
+describe('sign-up', function () {
+    var $httpBackend;
+    var ApiPath;
+
+    beforeEach(function () {
+        module('public');
+    });
+
+    var $controller;
+    var signUpController;
+    var $httpBackend;
+    var ApiPath;
+
+    beforeEach(inject(function (_$controller_, $injector) {
+        $controller = _$controller_;
+
+        signUpController =
+            $controller('SignUpController',
+                {});
+
+        $httpBackend = $injector.get('$httpBackend');
+        ApiPath = $injector.get('ApiPath');
+    }));
+
+    it('should return object when menu item exists', function () {
+        $httpBackend.whenGET(ApiPath + '/menu_items/abc/menu_items/3.json').respond({ 'foo': 'bar' });
+
+        signUpController.user.favoriteMenuItem = 'abc4';
+
+        signUpController.validateFavoriteItem().then(function (result) {
+            expect(result).toEqual({ category: 'abc', item: { 'foo': 'bar' } });
+            expect(signUpController.invalidFavoriteItem).toBe(false);
+        });
+
+        $httpBackend.flush();
+    });
+
+    it('should return null when favorite item does not match expected pattern (without any web request)', function () {
+        signUpController.user.favoriteMenuItem = '45ABC';
+
+        signUpController.validateFavoriteItem()
+            .then(function (result) {
+                expect(result).toEqual(null);
+                expect(signUpController.invalidFavoriteItem).toBe(true);
+            });
+
+        try {
+            $httpBackend.flush();
+        } catch {
+            // Ignore
+        }
+    });
+
+    it('should return null when menu item does not exist', function () {
+        $httpBackend.whenGET(ApiPath + '/menu_items/abc/menu_items/3.json').respond(null);
+
+        signUpController.user.favoriteMenuItem = 'abc4';
+
+        signUpController.validateFavoriteItem().then(function (result) {
+            expect(result).toEqual(null);
+            expect(signUpController.invalidFavoriteItem).toBe(true);
+        });
+
+        $httpBackend.flush();
+    });
+});
